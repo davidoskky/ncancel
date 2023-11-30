c323f2081fbefa27cfaed67be25f7aa0c1610fe8
davidoskky <davidoskky@yahoo.it>
2023-11-30 13:41:43 +0100

    Initial scrolling logic

--- ncurses.py
+++ ncurses.py
@@ -5,6 +5,9 @@ import subprocess
 def main(stdscr):
     curses.curs_set(0)  # Hide cursor
     stdscr.clear()  # Clear the screen
+
+    current_line = 0  # Current line position in job list
+    num_lines = curses.LINES - 4    
     while True:
         stdscr.clear()
         # Display header
@@ -12,14 +15,18 @@ def main(stdscr):
         # Fetch and display jobs
         jobs = subprocess.check_output(['squeue']).decode()
         stdscr.addstr(jobs)
+
+        for i in range(current_line, min(current_line + num_lines, len(job_lines))):
+            stdscr.addstr(job_lines[i] + '\n')
         # Handle key presses for scrolling and selecting
         k = stdscr.getch()
-        if k == curses.KEY_DOWN:
-            stdscr.clear()
-        elif k == curses.KEY_UP:
-            stdscr.clear()
+        if k == curses.KEY_DOWN and current_line < len(job_lines) - num_lines:
+            current_line += 1
+        elif k == curses.KEY_UP and current_line > 0:
+            current_line -= 1
         elif k == ord('d'):  # Example: 'd' for delete
-            stdscr.clear()
+            # Cancel job logic
+            pass
         # Refresh the screen
         stdscr.refresh()
         # Other logic
